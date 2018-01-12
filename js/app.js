b51461da4adea84b48cd0f3a9eb85e4261d11d14
David Johnson <djohnso08@yahoo.com>
2018-01-11 19:52:23 -0800

    added first question to app.js file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var correct = 0;
+console.log('Here lies some trickery questions! Beware of the trickery');
+
+alert('Welcome to my guessing game!');
+
+var user = prompt('What is your name?');
+alert('Welcome ' + user + '! Hope you enjoy my guessing game.');
+
+console.log('If you were running a race and you passed the person in 2nd place, what place would you be in now?');
