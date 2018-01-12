2bc6264de648871ed3246db329bf32cb909ac7e8
David Johnson <djohnso08@yahoo.com>
2018-01-11 20:53:33 -0800

    added third and fourth question to app.js

--- js/app.js
+++ js/app.js
@@ -5,15 +5,15 @@ console.log('Here lies some trickery questions! Beware of the trickery');
 
 alert('Welcome to my guessing game!');
 
-var user = prompt('What is your name?');
-alert('Welcome ' + user + '! Hope you enjoy my guessing game.');
+var userName = prompt('What is your name?');
+alert('Welcome ' + userName + '! Hope you enjoy my guessing game.');
 
 console.log('If you were running a race and you passed the person in 2nd place, what place would you be in now?');
 
-var answer = prompt('Is David\'s favorite movie Casablanca?' , 'Type yes or no').toLowerCase();
+var movie = prompt('Is David\'s favorite movie Casablanca?' , 'Type yes or no').toLowerCase();
 console.log('There was a red bungalow. Everything in it was red - the walls, the carpets, the furniture. What color were the stairs?');
 
-if (answer === 'yes' || answer === 'y') {
+if (movie === 'yes' || movie === 'y') {
     //if it's correct give them a point
     alert('You are correct!');
     correct++;
@@ -21,6 +21,45 @@ if (answer === 'yes' || answer === 'y') {
 else {
     //if it's not correct, tell them to try again
     alert('Incorrect. All the things of Humphrey Bogart!!!');
+}
+
+var dance = prompt('Have I ever won a dance contest?' , 'Type yes or no').toLowerCase();
+console.log('There was a red bungalow. Everything in it was red - the walls, the carpets, the furniture. What color were the stairs?');
+
+if (dance === 'yes' || dance === 'y') {
+    //if it's correct give them a point
+    alert('You are correct! ');
+    correct++;
+}
+else {
+    //if it's not correct, tell them to try again
+    alert('Incorrect. The judges on Dancing with the stars would no appreciate your answer!!!');
+}
+
+var diver = prompt('Have I dove deeper than 100ft?' , 'Type yes or no').toLowerCase();
+console.log('There was a red bungalow. Everything in it was red - the walls, the carpets, the furniture. What color were the stairs?');
+
+if (diver === 'yes' || diver === 'y') {
+    //if it's correct give them a point
+    alert('You are correct! It get\'s cold down there');
+    correct++;
+}
+else {
+    //if it's not correct, tell them to try again
+    alert('Nice try, but you are incorrect!!!');
+}
+
+var military = prompt('Was I in the military?' , 'Type yes or no').toLowerCase();
+console.log('There was a red bungalow. Everything in it was red - the walls, the carpets, the furniture. What color were the stairs?');
+
+if (military === 'yes' || military === 'y') {
+    //if it's correct give them a point
+    alert('You are correct!');
+    correct++;
+}
+else {
+    //if it's not correct, tell them to try again
+    alert('Not even close!!!');
 }
 
 alert('You got ' + correct + ' correct');
